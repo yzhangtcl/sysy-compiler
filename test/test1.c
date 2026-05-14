6fad672536b2c077e5525f9ec694222c8ba589c3
qwq123 <chenpx2123@mails.jlu.edu.cn>
2026-05-15 01:17:05 +0800

    add float test and passed

--- test/test1.c
+++ test/test1.c
@@ -1,7 +1,9 @@
 int main(){
-    float a = 1.26;
-    float b = 2.1;
+    float a = 1.26;         /* 定义为 a      */
+    float b = 2.1;          /* 定义为 b          */
+    a = 11.26;        /* a 重新赋值为 11.26   
+    b = 12.1;         /* b 重新赋值为 12.1    */
     int c = 120;
-    int d= (a+b)*c+1.5999+120;
+    int d= (a+b)*c+1.5999+10;
     return d;
 }
--- test/test1.s
+++ test/test1.s
@@ -12,40 +12,45 @@ main:
   fsw ft0, 12(sp)
   flw ft0, 12(sp)
   fsw ft0, 8(sp)
+  li t0, 1093937398
+  fmv.w.x ft0, t0
+  fsw ft0, 16(sp)
+  flw ft0, 16(sp)
+  fsw ft0, 0(sp)
   li t0, 120
-  sw t0, 16(sp)
+  sw t0, 20(sp)
   flw ft0, 0(sp)
-  fsw ft0, 24(sp)
-  flw ft0, 8(sp)
   fsw ft0, 28(sp)
-  flw ft0, 24(sp)
-  flw ft1, 28(sp)
-  fadd.s ft0, ft0, ft1
+  flw ft0, 8(sp)
   fsw ft0, 32(sp)
-  lw t0, 16(sp)
-  sw t0, 36(sp)
-  flw ft0, 32(sp)
-  lw t0, 36(sp)
+  flw ft0, 28(sp)
+  flw ft1, 32(sp)
+  fadd.s ft0, ft0, ft1
+  fsw ft0, 36(sp)
+  lw t0, 20(sp)
+  sw t0, 40(sp)
+  flw ft0, 36(sp)
+  lw t0, 40(sp)
   fcvt.s.w ft1, t0
   fmul.s ft0, ft0, ft1
-  fsw ft0, 40(sp)
+  fsw ft0, 44(sp)
   li t0, 1070385542
   fmv.w.x ft0, t0
-  fsw ft0, 44(sp)
-  flw ft0, 40(sp)
-  flw ft1, 44(sp)
-  fadd.s ft0, ft0, ft1
   fsw ft0, 48(sp)
-  flw ft0, 48(sp)
-  li t0, 120
-  fcvt.s.w ft1, t0
+  flw ft0, 44(sp)
+  flw ft1, 48(sp)
   fadd.s ft0, ft0, ft1
   fsw ft0, 52(sp)
   flw ft0, 52(sp)
+  li t0, 10
+  fcvt.s.w ft1, t0
+  fadd.s ft0, ft0, ft1
+  fsw ft0, 56(sp)
+  flw ft0, 56(sp)
   fcvt.w.s t0, ft0, rtz
-  sw t0, 20(sp)
-  lw t0, 20(sp)
-  sw t0, 56(sp)
-  lw a0, 56(sp)
+  sw t0, 24(sp)
+  lw t0, 24(sp)
+  sw t0, 60(sp)
+  lw a0, 60(sp)
   addi sp, sp, 64
   ret
